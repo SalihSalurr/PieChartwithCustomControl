5473b561e253dc132ced69618f8030ba1a064093
SalihSalurr <58612345+SalihSalurr@users.noreply.github.com>
2021-06-27 13:34:17 +0300

    V1

---
+++ PieChart/Proje2/My2DPie.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace Proje2
+{
+    partial class My2DPie
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Pink;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(35, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Değer-1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Aqua;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(35, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Değer-2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Yellow;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(35, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Değer-3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(126, 91);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(126, 129);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Orange;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(35, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Değer-4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.DarkRed;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(35, 216);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Değer-5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Magenta;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(35, 258);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 24);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Değer-6";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(126, 174);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(126, 216);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(126, 262);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 12;
+            // 
+            // My2DPie
+            // 
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Size = new System.Drawing.Size(604, 427);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
---
+++ PieChart/Proje2/My2DPie.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proje2
+{
+    public partial class My2DPie : Control
+    {
+        public My2DPie()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Control sınıfındaki OnPaint metodu override edilerek kontrolün görsel görünümü metot içerisinde gerçekleştiriliyor.
+        /// </summary>
+        /// <param name="pe"></param>
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            float d1 = 0, d2, d3, d4, d5, d6, total;
+
+            // Textbox içerisinde değer yoksa 0 varsa gelen değer değişkene eşitleniyor.
+            d1 = textBox1.Text == "" ? 0 : int.Parse(textBox1.Text);       
+            d2 = textBox2.Text == "" ? 0 : int.Parse(textBox2.Text);
+            d3 = textBox3.Text == "" ? 0 : int.Parse(textBox3.Text);
+            d4 = textBox4.Text == "" ? 0 : int.Parse(textBox4.Text);
+            d5 = textBox5.Text == "" ? 0 : int.Parse(textBox5.Text);
+            d6 = textBox6.Text == "" ? 0 : int.Parse(textBox6.Text);
+
+            // Yüzdelik hesabı yapmak için toplanıyor.
+            total = d1 + d2 + d3 + d4 + d5 + d6;                           
+
+            float pd1, pd2, pd3, pd4, pd5, pd6;
+
+            // Pastanın çevresine göre yüzdelik hesabı yapıyor.
+            pd1 = (d1 / total) * 360;                                      
+            pd2 = (d2 / total) * 360;
+            pd3 = (d3 / total) * 360;
+            pd4 = (d4 / total) * 360;
+            pd5 = (d5 / total) * 360;
+            pd6 = (d6 / total) * 360;
+
+            Pen p = new Pen(Color.Black, 4);
+
+            Graphics g = this.CreateGraphics();
+
+            //Textbox1 in lokasyonuna göre grafiğe lokasyon alarak çizdiriyor.
+            Rectangle rec = new Rectangle(textBox1.Location.X + textBox1.Size.Width + 60, 50, 250, 250);
+
+            // Her bir pasta değeri için farklı renk belirleniyor.
+            Brush b1 = new SolidBrush(Color.Pink);                         
+            Brush b2 = new SolidBrush(Color.Aqua);
+            Brush b3 = new SolidBrush(Color.Yellow);
+            Brush b4 = new SolidBrush(Color.Orange);
+            Brush b5 = new SolidBrush(Color.DarkRed);
+            Brush b6 = new SolidBrush(Color.Magenta);
+
+            g.Clear(Form1.DefaultBackColor);
+
+            // İlk değer sıfır değilse grafiği çizdirmeye başlıyor.
+            if (!(d1 == 0))         
+            {
+                // Kontrolün içerisindeki MyDrawPie metodu ile Pen, Rectangle, başlangıç ve bitiş açısını göndrerek dilimin sınırları çizdiriliyor.
+                MyDrawPie(p, rec, 0, pd1);
+                // Graphics sınıfının FillPie metodu ile Brush, Rectangle, başlangıç ve bitiş açısını göndrerek dilimin içi boyanıyor.
+                g.FillPie(b1, rec, 0, pd1);                                 
+                g.DrawPie(p, rec, pd1, pd2);
+                g.FillPie(b2, rec, pd1, pd2);
+                MyDrawPie(p, rec, pd1 + pd2, pd3);
+                g.FillPie(b3, rec, pd1 + pd2, pd3);
+                MyDrawPie(p, rec, pd1 + pd2 + pd3, pd4);
+                g.FillPie(b4, rec, pd1 + pd2 + pd3, pd4);
+                MyDrawPie(p, rec, pd1 + pd2 + pd3 + pd4, pd5);
+                g.FillPie(b5, rec, pd1 + pd2 + pd3 + pd4, pd5);
+                MyDrawPie(p, rec, pd1 + pd2 + pd3 + pd4 + pd5, pd6);
+                g.FillPie(b6, rec, pd1 + pd2 + pd3 + pd4 + pd5, pd6);
+            }
+            
+        }
+
+
+        /// <summary>
+        /// Bu metot içerisinde pastanın dış çemberi ve dilimleri belirleniyor. 
+        /// </summary>
+        /// <param name="pen">Çizim yapabilmek için gereken sınıf.</param>
+        /// <param name="rect">Pastanın hangi sınırlar içerisinde ve kontrolün hangi kısmında duracağının parametresi.</param>
+        /// <param name="startAngle">Çizilecek olan pasta dilimlerinin her birinin başlangıç açısı.</param>
+        /// <param name="sweepAngle">Çizilecek olan pasta dilimlerinin her birinin bitiş açısı.</param>
+        public void MyDrawPie(System.Drawing.Pen pen, System.Drawing.Rectangle rect, float startAngle, float sweepAngle)
+        {
+            int startX1, endX1, startY1, endY1, LineLength;
+
+            // Başlangıç kordinatlarını bulmak için
+            startX1 = rect.X + rect.Width / 2;                          
+            startY1 = rect.Y + rect.Height / 2;
+            // Yarıçapı bulmak için
+            LineLength = rect.Width / 2;                                
+
+            double DEGREES_TO_RADIANS = Math.PI / 180.0;
+            // Bitiş kordinatlarını bulmak için
+            endX1 = startX1 + (int)(LineLength * Math.Cos(startAngle * DEGREES_TO_RADIANS));      
+            endY1 = startY1 + (int)(LineLength * Math.Sin(startAngle * DEGREES_TO_RADIANS));
+
+
+            Graphics g = this.CreateGraphics();
+
+            // Graphics sınıfının DrawLine metoduna başlangıç ve bitiş kordinatlaro gönderilerek pastanın yan çizgileri çizdiriliyor.
+            g.DrawLine(pen, startX1, startY1, endX1, endY1);
+            // Graphics sınıfının DrawLarc metoduna başlangıç ve bitiş açıları gönderilerek pastanın arka yüzeyi çizdiriliyor. 
+            g.DrawArc(pen, rect, startAngle, sweepAngle);                
+
+        }
+    }
+}
